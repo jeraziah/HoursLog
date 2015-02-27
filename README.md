86330adcb25172f0953ef21f196c813bbb9d5723
Alec <stealth550@gmail.com>
2015-02-27 11:36:52 -0600

    java build stuff.

---
+++ README.md
@@ -0,0 +1,3 @@
+# HoursTracking
+hour tracking software with no auth and temporary database for 362
+Josh, Jeff, and Alec
---
+++ src/backend/Program.java
@@ -0,0 +1,11 @@
+package backend;
+
+public class Program {
+	public static void main(String args[])
+	{
+		
+		
+		
+		return;
+	}
+}
