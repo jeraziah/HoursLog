6f390fa1771d57c074e242e8d885858375aee8da
Alec <stealth550@gmail.com>
2015-03-02 10:35:38 -0600

    added check for month index.

--- src/backend/Employee.java
+++ src/backend/Employee.java
@@ -55,23 +55,29 @@ public class Employee implements User {
 	}
 	
 	public int convert(int month, int dayOfMonth) {
-		int result = 0;
-		
-		// Converts month and day integers into a single integer representing the day of the year.
-		if(month == 1) { result += dayOfMonth; }
-		if(month == 2) { result += (31 + dayOfMonth); }
-		if(month == 3) { result += (59 + dayOfMonth); }
-		if(month == 4) { result += (90 + dayOfMonth); }
-		if(month == 5) { result += (120 + dayOfMonth); }
-		if(month == 6) { result += (151 + dayOfMonth); }
-		if(month == 7) { result += (181 + dayOfMonth); }
-		if(month == 8) { result += (212 + dayOfMonth); }
-		if(month == 9) { result += (243 + dayOfMonth); }
-		if(month == 10) { result += (273 + dayOfMonth); }
-		if(month == 11) { result += (304 + dayOfMonth); }
-		if(month == 12) { result += (334 + dayOfMonth); }
-		
-		return result;
+		if(inMonth(month, dayOfMonth))
+		{
+			int result = 0;
+			
+			// Converts month and day integers into a single integer representing the day of the year.
+			if(month == 1) { result += dayOfMonth; }
+			if(month == 2) { result += (31 + dayOfMonth); }
+			if(month == 3) { result += (59 + dayOfMonth); }
+			if(month == 4) { result += (90 + dayOfMonth); }
+			if(month == 5) { result += (120 + dayOfMonth); }
+			if(month == 6) { result += (151 + dayOfMonth); }
+			if(month == 7) { result += (181 + dayOfMonth); }
+			if(month == 8) { result += (212 + dayOfMonth); }
+			if(month == 9) { result += (243 + dayOfMonth); }
+			if(month == 10) { result += (273 + dayOfMonth); }
+			if(month == 11) { result += (304 + dayOfMonth); }
+			if(month == 12) { result += (334 + dayOfMonth); }
+			
+			return result;
+		}
+		else{
+			throw new IndexOutOfBoundsException();
+		}
 	}
 	
 	public boolean inMonth(int month, int dayOfMonth) {
